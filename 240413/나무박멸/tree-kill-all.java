45560eebeee277bfd6a68143818e167a053267af
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2024-04-13 20:19:49 +0900

    Title: 나무박멸, Time: 184ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 240413/나무박멸/tree-kill-all.java
+++ 240413/나무박멸/tree-kill-all.java
@@ -71,12 +71,6 @@ public class Main {
 
 		for (int i = 0; i < n; i++)
 			map[i] = temp[i].clone();
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++)
-				System.out.print(map[i][j] + "\t");
-			System.out.println();
-		}
 	}
 
 	static Point findTree() {
